0c60aa206b5e2bc6e7d162a5cc252c0fbf62de39
ash2taf <ash2taf@virginia.edu>
2022-04-14 16:18:03 -0400

    Struggle is real
    I think this maybe works, but I based it on some of the changes you guys made to user id in other branches, so it fails on this branch still

--- templates/friendsPage.php
+++ templates/friendsPage.php
@@ -1,186 +1,167 @@
+<?php
+require('connect-db.php');
+require('friend_db.php');
 
+$list_of_friends = getAllFriends();
+$friend_to_update = null;
+$friend_to_delete = null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Add")
+    {  
+      // If the button is clicked and its value is "Add" then call addFriend() function
+
+      addFriend($_POST['u_id']);
+      $list_of_friends = getAllFriends();
+    }
+/*    else if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Update")
+    {  
+      // If the button is clicked and its value is "Update" then retrieve info about that friend.
+      // We'll later fill in the friend's info in the form so that a user can update the info.
+       
+      $friend_to_update = getFriend_byName($_POST['friend_to_update']);
+
+      // To fill in the form, assign the pieces of info to the value attributes of form input textboxes.
+      // Then, we'll wait until a user makes some changes to the friend's info 
+      // and click the "Confirm update" button to actually make it reflect the database. 
+      // (also note: "name" is a primary key -- refer to the friends table we created, thus can't be updated)
+    } */
+    else if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Delete")
+    {  
+      $friend_to_delete = getFriend_byName($_POST['friend_to_delete']);
+    }
+/*
+    if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Confirm Update")
+    {
+      updateFriend($_POST['name'], $_POST['major'], $_POST['year']);
+      $list_of_friends = getAllFriends();
+    }
+*/
+    if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Confirm Delete")
+    {
+      deleteFriend($_POST['u_id']);
+      $list_of_friends = getAllFriends();
+    }
+}
+?>
+
+
+<!-- 1. create HTML5 doctype -->
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <style type="text/css">
-    ci {
-    text-indent: 15px;
-    color: orange;
-    font-weight: bold;
-    }
-    ciA {
-    text-indent: 15px;
-    color: orange;
-    font-weight: bold;
-    }
-    td {
-      padding: 0px;
-    }
-  </style>
+  <meta charset="UTF-8">  
+  
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 
+  Bootstrap is designed to be responsive to mobile.
+  Mobile-first styles are part of the core framework.
+   
+  width=device-width sets the width of the page to follow the screen-width
+  initial-scale=1 sets the initial zoom level when the page is first loaded   
+  -->
+  
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">  
+    
+  <title>DB interfacing example</title>
+  
+  <!-- 3. link bootstrap -->
+  <!-- if you choose to use CDN for CSS bootstrap -->  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+  <!-- you may also use W3's formats -->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <!-- 
+  Use a link tag to link an external resource.
+  A rel (relationship) specifies relationship between the current document and the linked resource. 
+  -->
+  
+  <!-- If you choose to use a favicon, specify the destination of the resource in href -->
+  <link rel="icon" type="image/png" href="https://emoji.gg/assets/emoji/7812_check_mark.png" />
+  
+  <!-- if you choose to download bootstrap and host it locally -->
+  <!-- <link rel="stylesheet" href="path-to-your-file/bootstrap.min.css" /> --> 
+  
+  <!-- include your CSS -->
+  <!-- <link rel="stylesheet" href="custom.css" />  -->
+       
 </head>
-<body class="large-screen-container" style="background-color: #4a524d">
-  <!-- Intro and picture-->
-  <div class="pt-4 container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="homepagebody" style="color:white">
-          <h4 class="text-center">Welcome to your MTG Wishlist <?php echo $_SESSION["username"]; ?></h4>
-          <form name="viewCards" method="post">
-		        <div class="row mb-3 mx-3">
-                <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:30px">Optional Filters:</p>
-                <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Set Rarity:</p>
-                <select name="rarities" style="width:1000px;margin-left:auto;margin-right:auto">
-                  <option disabled selected value> </option>
-                  <option name="common">Common</option>
-                  <option name="uncommon">Uncommon</option>
-                  <option name="rare">Rare</option>
-                  <option name="mythic">Mythic</option>
-                </select>
-                <p></p>
-                <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Set Mana Cost Range:</p>
-                <table>
-                  <tr>
-                    <td style="color:orange;padding-left:235px">Minimum Cost </td>
-                    <td style="color:orange">Maximum Cost </td>
-                  </tr>
-                  <tr>
-                    <td style="padding-left:235px"><input type="number" class="form-control" name="minMana" style="width:500px"/></td>
-                    <td style ="padding-right:235px"><input type="number" class="form-control" name="maxMana" style="width:500px"/></td>
-                  </tr>
-                </table>
-                <p></p>
-                <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Set Price Range:</p>
-                <table>
-                  <tr>
-                    <td style="color:orange;padding-left:235px">Minimum Price </td>
-                    <td style="color:orange">Maximum Price </td>
-                  </tr>
-                  <tr>
-                    <td style="padding-left:235px"><input type="number" step="0.01"  class="form-control" name="minPrice" style="width:500px"/></td>
-                    <td style ="padding-right:235px"><input type="number" step="0.01" class="form-control" name="maxPrice" style="width:500px"/></td>
-                  </tr>
-                </table>
-                <p></p>
-				        <input class="btn btn-info" type="submit" name="viewAction" value="View Wishlist" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-		        </div>
-	        </form>
-          <form name="addCard" method="post">
-            <div class="row mb-3 mx-3">
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:30px">Add a Card to your Wishlist:</p>
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Enter Card Number:</p>
-              <input type="number" class="form-control" name="cardNumber" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-              <p></p>
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Enter Set Number:</p>
-              <input type="number" class="form-control" name="setNumber" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-              <p></p>
-              <input class="btn btn-success" type="submit" name="addCardAction" value="Add Card" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-            </div>
-          </form>
-          <form name="deleteCard" method="post">
-            <div class="row mb-3 mx-3">
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:30px">Remove a Card from your Wishlist:</p>
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Enter Card Number:</p>
-              <input type="number" class="form-control" name="cardNumber2" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-              <p></p>
-              <p class="font-weight-bold" style="font-weight:bold;color:orange;font-size:15px;margin-left:225px">Enter Set Number:</p>
-              <input type="number" class="form-control" name="setNumber2" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-              <p></p>
-              <input class="btn btn-danger" type="submit" name="removeCardAction" value="Remove Card" required style="width:1000px;margin-left:auto;margin-right:auto"/>
-            </div>
-          </form>
-          <?php
-            if (isset($_POST["addCardAction"])){
-              $this->db->query("insert into card_on_wishlist values(" . $_SESSION["id"] . ", " . $_POST["cardNumber"] . ", " . $_POST["setNumber"] . ")");
-            }
-            if (isset($_POST["removeCardAction"])){
-              $this->db->query("delete from card_on_wishlist where u_id=" . $_SESSION["id"] . " and cn=" . $_POST["cardNumber2"] . " and s_id=" . $_POST["setNumber2"] . ";");
-            }
-            if(isset($_POST["viewAction"]))
-            {
-              if (isset($_POST["rarities"]) or isset($_POST["minMana"]) or isset($_POST["maxMana"])){
-                $conds = "";
-                if (isset($_POST["rarities"])){
-                  $conds = "and rarity=" . "'" . $_POST["rarities"] . "' " . $conds;
-                }
-                if ($_POST["minMana"] != ''){
-                  $conds = "and manaValue>="  . $_POST["minMana"] . " " . $conds;
-                }
-                if ($_POST["maxMana"]!= ''){
-                  $conds = "and manaValue<=" . $_POST["maxMana"] . " " . $conds;
-                }
-                if ($_POST["minPrice"] != ''){
-                  $conds = "and price>="  . $_POST["minPrice"] . " " . $conds;
-                }
-                if ($_POST["maxPrice"]!= ''){
-                  $conds = "and price<=" . $_POST["maxPrice"] . " " . $conds;
-                }
-                $conds = trim($conds) . ";";
-                $data = $this->db->query("select * from (cards c JOIN card_on_wishlist cw ON c.cn=cw.cn AND c.setCode=cw.s_id) where u_id =" . $_SESSION["id"] . " " . $conds); 
-              }
-              else{
-                $data = $this->db->query("select * from card_on_wishlist where u_id = ?;", "i", $_SESSION["id"]);
-              }
-              $leng = count($data);
-              if ($leng==0){
-                  echo "<html>";
-                  echo "<p style='color:orange;margin-left:525px;font-size:30px;font-weight:bold'>Add some cards to your wishlist!<p>";
-                  echo "</html>";
-              }
-              else{
-                  $notPrint = array('name', 'rarity');
-                  $convToDisp = array('cn'=>'Card Number', 
-                                      'artist'=>'Artist', 
-                                      'cardindex'=>'Card Index',
-                                      'colors'=>'Colors', 
-                                      'convertedManaCost'=>'Converted Mana Cost',
-                                      'flavor_text'=> 'Flavor Text',
-                                      'loyalty'=>'Loyalty',
-                                      'manaCost'=>'Mana Cost',
-                                      'manaValue'=>'Mana Value',
-                                      'mtgjsonV4Id'=>'MTGJSON V4ID',
-                                      'multiverseId'=>'Multiverse ID',
-                                      'power'=>'Power',
-                                      'price'=>'Price',
-                                      'rules_text'=>'Rules Text',
-                                      'setCode'=>'Set Code',
-                                      'subtypes'=>'Subtypes',
-                                      'toughness'=>'Toughness',
-                                      'types'=>'Types');
-                  $rarToCol = array('common'=> 'black', 'uncommon'=> 'silver', 'rare'=>'gold', 'mythic'=>'#CD7F32');
-                  for($c=0; $c < $leng; $c++){
-                      $s_data = $this->db->query("select * from cards where cn = ? AND setCode = ?", "ii", $data[$c]['cn'], $data[$c]['s_id']);
-                      $c1 = $c + 1;
-                      $cName = $s_data[0]['name'];
-                      $cCol = $rarToCol[$s_data[0]['rarity']];
-                      echo "<html>";
-                      echo "<h1 style='color:$cCol'> $cName \n \n </h1>";
-                      echo "</html>";
-                      foreach($s_data[0] as $cardK => $cardV){
-                        if (!in_array($cardK, $notPrint)){
-                          $disp = $convToDisp[$cardK];
-                          echo "<html>";
-                          echo "<head>";
-                          echo "</head>";
-                          echo "<div class='row'>";
-                          echo "<ci> $disp: $cardV \n </ci>";
-                          echo "</div>";
-                          echo "</html>";
-                        }
-                      }
-                      echo nl2br("\n");   
-                  }
-              }
-            }
-          ?>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--Body paragraphs-->
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/less@4"></script>
-</body>
 
+<body>
+<div class="container">
+  <h1>Friend book</h1>  
+
+  <form name="mainForm" action="simpleform.php" method="post">   
+  <div class="row mb-3 mx-3">
+    Name:
+    <input type="text" class="form-control" name="u_id" required 
+            value="<?php if ($friend_to_update!=null) echo $friend_to_update['u_id']; else if ($friend_to_delete!=null) echo $friend_to_delete['name'] ?>"
+    />        
+  </div>  
+  <!--<div class="row mb-3 mx-3">
+    Year:
+    <input type="number" class="form-control" name="year" required min="1" max="4" 
+            value="< ?php if ($friend_to_update!=null) echo $friend_to_update['year']; else if ($friend_to_delete!=null) echo $friend_to_delete['year'] ?>"
+    /> 
+  </div>  
+  <div class="row mb-3 mx-3">
+    Major:
+    <input type="text" class="form-control" name="major" required 
+            value="< ?php if ($friend_to_update!=null) echo $friend_to_update['major']; else if ($friend_to_delete!=null) echo $friend_to_delete['major'] ?>"
+    />
+  </div> -->  
+  <input type="submit" value="Add" name="btnAction" class="btn btn-dark" 
+        title="insert a friend" />  
+  <!--<input type="submit" value="Confirm Update" name="btnAction" class="btn btn-dark" 
+        title="update info" />   -->
+  <input type="submit" value="Confirm Delete" name="btnAction" class="btn btn-danger" 
+        title="delete info" />  
+</form>    
+
+<hr/>
+<h2>List of Friends</h2>
+<!-- <div class="row justify-content-center">   -->
+<table class="w3-table w3-bordered w3-card-4" style="width:90%">
+  <thead>
+  <tr style="background-color:#B0B0B0">
+    <th width="80%">User ID</th>
+    <th width="20%">Delete ?</th> 
+  </tr>
+  </thead>
+  <?php foreach ($list_of_friends as $friend): ?>
+  <tr>
+    <td><?php echo $friend['u_id']; ?></td>
+    <td>
+      <form action="friendsPage.php" method="post">
+        <input type="submit" value="Update" name="btnAction" class="btn btn-primary" />
+        <input type="hidden" name="friend_to_update" value="<?php echo $friend['u_id'] ?>" />      
+      </form>
+    </td>
+    <td>
+      <form action="friendsPage.php" method="post">
+        <input type="submit" value="Delete" name="btnAction" class="btn btn-danger" />
+        <input type="hidden" name="friend_to_delete" value="<?php echo $friend['u_id'] ?>" />      
+      </form>
+    </td> 
+  </tr>
+  <?php endforeach; ?>
+
+  
+  </table>
+<!-- </div>   -->
+
+
+  <!-- CDN for JS bootstrap -->
+  <!-- you may also use JS bootstrap to make the page dynamic -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+  
+  <!-- for local -->
+  <!-- <script src="your-js-file.js"></script> -->  
+  
+</div>    
+</body>
 </html>
