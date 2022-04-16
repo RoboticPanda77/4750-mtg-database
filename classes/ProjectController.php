9f112b219b75600b84de81f8f317efeb274f8222
KevinMoritz <kmm9sz@virginia.edu>
2022-04-16 12:07:29 -0400

    Card Collection basic stuff, add cards to user who is signed in, no access to pages without sign in

--- classes/ProjectController.php
+++ classes/ProjectController.php
@@ -14,30 +14,69 @@ class ProjectController
 
     private function isLoggedIn()
     {
-        if (!isset($_SESSION["email"])) {
+        if (!isset($_SESSION["loggedin"])) {
+            session_start();
             $_SESSION['badlogaccess'] = true;
             header('Location: /4750-mtg-database/index.php');
+            exit();
         }
        
     }
 
     public function welcome() {
-
+        $this->isLoggedIn();
         include("templates/header.php");
         include("templates/welcome.php");
         include("templates/footer.php");
     }
-
-    public function upload_card() {
-        
-        $thisRan = "america";
+    public function collection() {
+        $this->isLoggedIn();
         if(isset($_POST["searchforcard"])) {
             $searchString = "%" . $_POST["searchforcard"] . "%";
-            $data = $this->db->query("select * from cards where name LIKE ?;", "s", $searchString);
+            $data = $this->db->query("select * from cards NATURAL JOIN owns_card JOIN sets ON 
+            cards.s_id = sets.s_id where name LIKE ? AND u_id = ?", "si", $searchString, $_SESSION["id"]);
+            $thisRan = $searchString;
+        } else {
+            $data = $this->db->query("select * from cards NATURAL JOIN owns_card JOIN sets ON cards.s_id = sets.s_id where u_id = ?", "i", $_SESSION["id"]);
+        }
+        include("templates/header.php");
+        include("templates/collection.php");
+        include("templates/footer.php");
+    }
+    public function friends() {
+        $this->isLoggedIn();
+        $friendsList = $this->db->query("select * from users NATURAL JOIN friends_with where
+         u_id = u_id1 AND u_id1 = ?", "i", $_SESSION["u_id"]);
+
+        include("templates/header.php");
+        include("templates/friendsPage.php");
+        include("templates/footer.php");
+    }
+
+    public function upload_card() {
+        $this->isLoggedIn();
+        if(isset($_POST["searchforcard"])) {
+            $searchString = "%" . $_POST["searchforcard"] . "%";
+            $data = $this->db->query("select * from cards where name LIKE ?", "s", $searchString);
             $thisRan = $searchString;
         } else {
             $data = $this->db->query("select * from cards");
+        }
+        if(isset($_GET["cn"])) {
             
+            $card_to_insert = $this->db->query("select * from cards where cn = ? AND 
+            s_id = ?;", "ii", $_GET["cn"], $_GET["s_id"]);
+            $user_owns = $this->db->query("select * from owns_card WHERE u_id = ? AND cn = ? AND s_id = ?
+            ;", "iii", $_SESSION["id"], $_GET["cn"], $_GET["s_id"]);
+            if(isset($user_owns[0]["cn"])) {
+                $inserted = "goodbye";
+                $this->db->query("UPDATE owns_card SET count = ? WHERE u_id = ? AND cn = ? AND s_id = ?",
+                "iiii", $user_owns[0]["count"] + 1, $_SESSION["id"], $_GET["cn"], $_GET["s_id"]);
+            } else {
+                $this->db->query("insert into owns_card VALUES (?, ?, ?, ?);", "iiii",
+                $_SESSION["id"], $_GET["cn"], 1, $_GET["s_id"]);
+            }
+            $_SESSION["add_success"] = true;
         }
         include("templates/header.php");
         include("templates/upload-card.php");
@@ -56,7 +95,9 @@ class ProjectController
         //print_r($data); <-- useful print function for arrays
         include("templates/footer.php");
     }
+    
     public function packs() {
+        $this->isLoggedIn();
         #$data = $this->db->query("select * from packs natural join sets where u_id = ?;", "i", 1);
         $data = $this->db->query("select * from packs natural join sets natural join users where username = ?;", "s", Config::$db["user"]);
         include("templates/header.php");
@@ -65,11 +106,31 @@ class ProjectController
         include("templates/footer.php");
     }
     public function get_pack($packnum) {
-        $data = $this->db->query("select * from pack_contains natural join users natural join cards where username = ? and p_num = ?;", "si", Config::$db["user"], $packnum);
+        $this->isLoggedIn();
+        $data = $this->db->query("select * from pack_contains natural join cards where u_id = ? and p_num = ?;", "si", $_SESSION["u_id"], $packnum);
         include("templates/header.php");
         include("templates/single-pack.php");
         //print_r($data); <-- useful print function for arrays
         include("templates/footer.php");
+    }
+
+    public function wishList(){
+        $this->isLoggedIn();
+        include("templates/header.php");
+        include("templates/wishListPage.php ");
+        include("templates/footer.php");
+    }
+
+    public function logIn() {
+        if (isset($_SESSION["badlogaccess"])) {
+            
+            $loggedMsg = "Please log in or create an account to use the full functionality of the UVA MtG Database!";
+            echo "<script type='text/javascript'>alert('$loggedMsg')</script>";
+            unset($_SESSION["badlogaccess"]);
+            session_destroy();
+        }
+
+        include("templates/login.php");
     }
 
     public function run()
@@ -87,6 +148,15 @@ class ProjectController
                 break;
             case "upload_card":
                 $this->upload_card();
+                break;
+            case "wish":
+                $this->wishList();
+                break;
+            case "logIn":
+                $this->logIn();
+                break;
+            case "collection":
+                $this->collection();
                 break;
             default:
                 $this->welcome();
--- templates/collection.php
+++ templates/collection.php
@@ -4,8 +4,8 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="homepagebody" style="color:white">
-                    <h4 class="text-center">Upload a New Card</h4>
-                    <p class="text-center">You can search for and add cards that you own here!</p>
+                    <h4 class="text-center">Your Card Collection</h4>
+                    <p class="text-center">View your hard-earned cards here!</p>
                     <?php if(isset($_SESSION["add_success"])) {
                         unset($_SESSION["add_success"]);
                         echo "<h3 class = \"text-center\" style = \"color: red\">Card Successfully Added!</h3>";
@@ -17,22 +17,22 @@
     </div>
 
     <!--Body paragraphs-->
-    <div class="pt-5 text-center" style="color:white">
-        <h4>Search for a card from our database that you want to add by name!</h4>
+    <div class="pt-5 text-center" style="color:white"> 
+    <h4>Search for cards by name or use another sorting option!</h4>
         <div class="pt-4 container-fluid">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-3">
 
                 </div>
                 <div class="col-md-4 justify-content-center text-center">
                     <div class="input-group">
-                        <form action="?command=upload_card" method="post" class="form-group">
+                        <form action="?command=collection" method="post" class="form-group">
                             <div class="input-group">
                                 <input type="text" class="form-control rounded" name="searchforcard" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
                                 <button type="submit" class="btn btn-outline-primary">Search</button>
                             </div>
                         </form>
-                        <a class="btn btn-danger mx-2" href="?command=upload_card" role="button">Clear Search Bar</a>
+                        <a class="btn btn-danger mx-2" href="?command=collection" role="button">Clear Search Bar</a>
                     </div>
                 </div>
             </div>
@@ -46,7 +46,8 @@
                         
                         <th>Card Name</th>
                         <th>Card Artist</th>
-                        <th>Add Card</th>
+                        <th>Set</th>
+                        <th>Cards Owned</th>
                         <?php 
                         //print_r($data);
                         //echo $inserted;
@@ -54,9 +55,8 @@
                             echo "<tr>";
                             echo "<td>" . $card["name"] . "</td>";
                             echo "<td>" . $card["artist"] . "</td>";
-                            echo "<td> <form action = \"?command=upload_card&cn=". $card["cn"] . "&s_id=".
-                            $card["s_id"] . "\" method=\"post\">
-                                  <button type=\"submit\" class=\"btn btn-outline-primary\">Add</button></form></td>";
+                            echo "<td>" . $card["s_name"] . "</td>";
+                            echo "<td>" . $card["count"] . "</td>";
                             echo "</tr>";
                         }
                         ?>
